fa243eb60fc73152aaecde14fd46cba8a10aeb6e
newspecies <harsh21592@gmail.com>
2018-09-06 16:48:28 +0530

    added more cluster mtrics and metric handloer

--- src/PromClient.js
+++ src/PromClient.js
@@ -16,3 +16,44 @@ exports["incrementCounter'"] = function (counter, labels) {
     return counter.inc(labels);
   };
 };
+
+exports.promMetricsHandler = promBundle({
+  httpDurationMetricName: 'euler_http_request_duration',
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 20, 30],
+  includeMethod: true,
+  includePath: true,
+  autoRegister: false,
+  customLabels: {
+    host: null,
+    instance: null,
+    pid: null,
+    merchant_id: null
+  },
+  transformLabels: function (labels) {
+    var _a = arguments[1] || {};
+    return Object.assign(labels, {
+      pid: process.pid,
+      instance: os.hostname(),
+      host: _a.hostname,
+      merchant_id: _a.body.merchantId || _a.body.merchant_id || _a.params.merchantId
+        || _a.params.merchant_id || _a.query.merchantId || _a.query.merchant_id
+        || _a.userData.merchantId || _a.userData.merchant_id || null
+    });
+  },
+  promClient: {
+    collectDefaultMetrics: {
+      timeout: 1000
+    }
+  },
+  urlValueParser: {
+    minHexLength: 5,
+    extraMasks: [
+      "^[0-9]+\\.[0-9]+\\.[0-9]+$" // replace dot-separated dates with #val
+    ]
+  },
+  normalizePath: [
+    ['\\?(.*)', '']
+  ]
+});
+
+exports.promClusterMetrics = promBundle.clusterMetrics(); 
--- src/PromClient.purs
+++ src/PromClient.purs
@@ -5,10 +5,13 @@ import Data.Function.Uncurried (Fn2, Fn3, runFn2, runFn3)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Aff (Aff)
-import Data.Foreign.Class (class Decode, class Encode, encode)
+import Data.Foreign.Class (class Encode, encode)
+import Node.Express.Types (ExpressM, Response, Request)
 
 foreign import data Metric :: Type
 foreign import data Timer :: Type
+foreign import promClusterMetrics :: forall e. Fn3 Request Response (ExpressM e Unit) (ExpressM e Unit)
+foreign import promMetricsHandler :: forall e. Fn3 Request Response (ExpressM e Unit) (ExpressM e Unit)
 
 foreign import initCounter' :: forall e. Fn3 String String (Array String) (Eff e Metric)
 foreign import incrementCounter' :: forall a e. Fn2 Metric a (Eff e Metric)
